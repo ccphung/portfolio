0a31066ec0a22a05bc4fd8afcd37e7cc3a86485b
Cédric Phung <cedric3011@outlook.fr>
2025-08-30 15:48:49 +0200

    modify file

--- i18n.js
+++ i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import commonEn from './locales/en/common.json';
-import commonFr from './/locales/fr/common.json';
+import commonEn from './src/locales/en/common.json';
+import commonFr from './src/locales/fr/common.json';
 
 i18n.use(initReactI18next).init({
   resources: {
